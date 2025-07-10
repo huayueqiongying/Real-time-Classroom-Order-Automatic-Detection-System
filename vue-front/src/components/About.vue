d51196fa77b416663e07f929db92aa8a4750f47c
huayueqiongying <144340345+huayueqiongying@users.noreply.github.com>
2025-07-10 10:46:24 +0800

    Add files via upload

---
+++ vue-front/src/components/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h2>关于我们</h2>
+    <p>我不知道写什么</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style scoped>
+.about {
+  margin: 20px auto;
+  max-width: 500px;
+  padding: 20px;
+  border: 1px solid #eee;
+  border-radius: 5px;
+}
+</style>
